765e2ae9d64b272a66665acdc7e343feaa4c77c8
Benjamin <boglebenjamin@gmail.com>
2017-06-09 13:24:28 -0500

    Create Title Case a Sentence.js

---
+++ Basic Algorithm Scripting/Title Case a Sentence.js
@@ -0,0 +1,12 @@
+function titleCase(str) {
+  
+  str = str.toLowerCase().split(" ");
+  
+  for(var i = 0; i < str.length; i++){
+    str[i] = str[i][0].toUpperCase() + str[i].slice(1);
+  }
+  
+  return str.join(' ');
+}
+
+titleCase("I'm a little tea pot");
